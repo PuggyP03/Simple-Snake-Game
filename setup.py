d5b147fe5e2603422c73a0a1c1d2557bc83e3e5a
Anthony Venable <72991293+PuggyP03@users.noreply.github.com>
2024-09-28 23:19:46 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Snake-Game-Package',
+    version='0.1.0',
+    py_modules=['game'],
+    description='A simple snake game',
+    author='Anthony Venable',
+    author_email='your.email@example.com',
+    url='https://github.com/PuggyP03/Simple-Snake-Game/new/dev',
+)
